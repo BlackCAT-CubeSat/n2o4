cb5005c11b2a5291bbdbe939836fff86da29b756
Zachary Catlin <zec0@psu.edu>
2021-09-14 16:37:50 -0400

    Build script: path, organization improvements
    Re paths: some things will work better if absolute paths are used.
    That's why this commit is using $CARGO_MANIFEST_DIR and much PathBuf.

--- build.rs
+++ build.rs
@@ -1,7 +1,33 @@
 extern crate bindgen;
 
 use std::env;
-use std::path;
+use std::path::PathBuf;
+
+fn main() {
+    let in_dir = env_unwrap("CARGO_MANIFEST_DIR");
+    let out_dir = env_unwrap("OUT_DIR");
+
+    let c_header_file = pb(&[&in_dir,  "cfs-all.h"]).to_string_unwrap();
+    let out_file      = pb(&[&out_dir, "cfs-all.rs"]).to_string_unwrap();
+    let deps_file     = pb(&[&out_dir, "cfs-all.d"]);
+
+    println!("cargo:rerun-if-changed={}", &c_header_file);
+
+    let bindings = bindgen::builder()
+        .header(&c_header_file)
+        .clang_args(env_unwrap("RUST_CFS_SYS_COMPILE_DEFINITIONS")
+            .split('@').map(|s| { String::from("-D") + s }))
+        .clang_args(env_unwrap("RUST_CFS_SYS_INCLUDE_DIRECTORIES")
+            .split('@').map(|s| { String::from("-I") + s }))
+        .clang_args(env_unwrap("RUST_CFS_SYS_COMPILE_OPTIONS").split('@'))
+        .allowlist_recursively(true)
+        .use_core()
+        .depfile(&out_file, deps_file)
+        .generate().expect("Unable to generate cFS bindings");
+
+    bindings.write_to_file(&out_file)
+        .expect("Unable to write out cFS bindings");
+}
 
 fn env_unwrap(key: &str) -> String {
     println!("cargo:rerun-if-env-changed={}", key);
@@ -9,23 +35,19 @@ fn env_unwrap(key: &str) -> String {
         .expect(&format!("Environment variable {} non-existent or unusable", key))
 }
 
-fn main() {
+/// Given a slice of path components, return the corresponding [`PathBuf`].
+fn pb(components: &[&str]) -> PathBuf {
+    let mut path = PathBuf::from(components[0]);
+    path.extend(&components[1..]);
+    path
+}
 
-    println!("cargo:rerun-if-changed=cfs-all.h");
+trait PathBufExt {
+    fn to_string_unwrap(&self) -> String;
+}
 
-    let bindings = bindgen::builder()
-        .header("cfs-all.h")
-        .clang_args(env_unwrap("RUST_CFS_SYS_COMPILE_DEFINITIONS")
-            .split('@').map(|s| { String::from("-D") + s }))
-        .clang_args(env_unwrap("RUST_CFS_SYS_INCLUDE_DIRECTORIES")
-            .split('@').map(|s| { String::from("-I") + s }))
-        .clang_args(env_unwrap("RUST_CFS_SYS_COMPILE_OPTIONS").split('@'))
-        //.use_core()
-        .generate().expect("Unable to generate cFS bindings");
-
-    let mut out_file = path::PathBuf::from(env_unwrap("OUT_DIR"));
-    out_file.push("cfs-all.rs");
-
-    bindings.write_to_file(&out_file)
-        .expect("Unable to write out cFS bindings");
+impl PathBufExt for PathBuf {
+    fn to_string_unwrap(&self) -> String {
+        self.to_str().unwrap().to_owned()
+    }
 }
