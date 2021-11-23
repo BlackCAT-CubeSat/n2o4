d3033ed22bba37e2e56bb7bfd5f309d69f24e75f
Zachary Catlin <zec0@psu.edu>
2021-11-23 11:28:58 -0500

    Started making wrapper type for SB pipes

--- src/cfe/sb.rs
+++ src/cfe/sb.rs
@@ -1,6 +1,8 @@
 // Copyright (c) 2021 The Pennsylvania State University. All rights reserved.
 
 use cfs_sys::*;
+use printf_wrap::NullString;
+use super::Status;
 
 pub use cfs_sys::CFE_SB_MsgId_Atom_t as MsgId_Atom;
 
@@ -28,5 +30,48 @@ impl From<MsgId> for MsgId_Atom {
     #[inline]
     fn from(id: MsgId) -> Self {
         unsafe { SHIM_CFE_SB_MsgIdToValue(id.id) }
+    }
+}
+
+#[derive(Debug)]
+pub struct Pipe { pub id: CFE_SB_PipeId_t }
+
+impl Pipe {
+    #[inline]
+    pub fn new(depth: u16, pipe_name: NullString) -> Result<Pipe, Status> {
+        let mut p: CFE_SB_PipeId_t = super::ResourceId::UNDEFINED.id;
+
+        let s: Status = unsafe {
+            CFE_SB_CreatePipe(&mut p, depth, pipe_name.as_ptr())
+        }.into();
+
+        if p == super::ResourceId::UNDEFINED.id {
+            return Err(Status::SB_PIPE_CR_ERR);
+        }
+
+        s.as_result(|| { Pipe { id: p } })
+    }
+
+    #[inline]
+    pub fn delete(self) {
+        unsafe { CFE_SB_DeletePipe(self.id); }
+    }
+
+    #[inline]
+    pub fn subscribe(&mut self, msg_id: MsgId) -> Result<(), Status> {
+        let s: Status = unsafe {
+            CFE_SB_Subscribe(msg_id.id, self.id)
+        }.into();
+
+        s.as_result(|| { () })
+    }
+
+    #[inline]
+    pub fn subscribe_local(&mut self, msg_id: MsgId, msg_lim: u16) -> Result<(), Status> {
+        let s: Status = unsafe {
+            CFE_SB_SubscribeLocal(msg_id.id, self.id, msg_lim)
+        }.into();
+
+        s.as_result(|| { () })
     }
 }
