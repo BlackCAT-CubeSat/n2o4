aae752a592f5de8642644d172fd71cb79d99bde5
Zachary Catlin <zec0@psu.edu>
2021-12-17 13:02:32 -0500

    Consolidated definitions of shims: one location, multiple users

--- cfs-shims.h
+++ cfs-shims.h
@@ -8,29 +8,46 @@
 #include <cfe.h>
 #include <osapi.h>
 
-unsigned long SHIM_CFE_ResourceId_ToInteger(CFE_ResourceId_t id);
-CFE_ResourceId_t SHIM_CFE_ResourceId_FromInteger(unsigned long Value);
-bool SHIM_CFE_ResourceId_Equal(CFE_ResourceId_t id1, CFE_ResourceId_t id2);
-bool SHIM_CFE_ResourceId_IsDefined(CFE_ResourceId_t id);
-bool SHIM_CFE_SB_MsgId_Equal(CFE_SB_MsgId_t MsgId1, CFE_SB_MsgId_t MsgId2);
-CFE_SB_MsgId_Atom_t SHIM_CFE_SB_MsgIdToValue(CFE_SB_MsgId_t MsgId);
-CFE_SB_MsgId_t SHIM_CFE_SB_ValueToMsgId(CFE_SB_MsgId_Atom_t MsgIdValue);
-int64 SHIM_OS_TimeGetTotalSeconds(OS_time_t tm);
-int64 SHIM_OS_TimeGetTotalMilliseconds(OS_time_t tm);
-int64 SHIM_OS_TimeGetTotalMicroseconds(OS_time_t tm);
-int64 SHIM_OS_TimeGetTotalNanoseconds(OS_time_t tm);
-int64 SHIM_OS_TimeGetFractionalPart(OS_time_t tm);
-uint32 SHIM_OS_TimeGetSubsecondsPart(OS_time_t tm);
-uint32 SHIM_OS_TimeGetMillisecondsPart(OS_time_t tm);
-uint32 SHIM_OS_TimeGetMicrosecondsPart(OS_time_t tm);
-uint32 SHIM_OS_TimeGetNanosecondsPart(OS_time_t tm);
-OS_time_t SHIM_OS_TimeAssembleFromNanoseconds(int64 seconds, uint32 nanoseconds);
-OS_time_t SHIM_OS_TimeAssembleFromMicroseconds(int64 seconds, uint32 microseconds);
-OS_time_t SHIM_OS_TimeAssembleFromMilliseconds(int64 seconds, uint32 milliseconds);
-OS_time_t SHIM_OS_TimeAssembleFromSubseconds(int64 seconds, uint32 subseconds);
-OS_time_t SHIM_OS_TimeAdd(OS_time_t time1, OS_time_t time2);
-OS_time_t SHIM_OS_TimeSubtract(OS_time_t time1, OS_time_t time2);
-unsigned long SHIM_OS_ObjectIdToInteger(osal_id_t object_id);
-osal_id_t SHIM_OS_ObjectIdFromInteger(unsigned long value);
-bool SHIM_OS_ObjectIdEqual(osal_id_t object_id1, osal_id_t object_id2);
-bool SHIM_OS_ObjectIdDefined(osal_id_t object_id);
+/* The following macros shim<n> (if not pre-defined)
+ * generate prototypes for shim functions of <n> arguments.
+ */
+
+#if !defined(shim1)
+#define shim1(rettype, fname, arg1type, arg1) \
+    rettype SHIM_ ## fname(arg1type arg1);
+#endif
+
+#if !defined(shim2)
+#define shim2(rettype, fname, arg1type, arg1, arg2type, arg2) \
+    rettype SHIM_ ## fname(arg1type arg1, arg2type arg2);
+#endif
+
+shim1(unsigned long, CFE_ResourceId_ToInteger, CFE_ResourceId_t, id)
+shim1(CFE_ResourceId_t, CFE_ResourceId_FromInteger, unsigned long, Value)
+shim2(bool, CFE_ResourceId_Equal, CFE_ResourceId_t, id1, CFE_ResourceId_t, id2)
+shim1(bool, CFE_ResourceId_IsDefined, CFE_ResourceId_t, id)
+
+shim2(bool, CFE_SB_MsgId_Equal, CFE_SB_MsgId_t, MsgId1, CFE_SB_MsgId_t, MsgId2)
+shim1(CFE_SB_MsgId_Atom_t, CFE_SB_MsgIdToValue, CFE_SB_MsgId_t, MsgId)
+shim1(CFE_SB_MsgId_t, CFE_SB_ValueToMsgId, CFE_SB_MsgId_Atom_t, MsgIdValue)
+
+shim1(int64, OS_TimeGetTotalSeconds, OS_time_t, tm)
+shim1(int64, OS_TimeGetTotalMilliseconds, OS_time_t, tm)
+shim1(int64, OS_TimeGetTotalMicroseconds, OS_time_t, tm)
+shim1(int64, OS_TimeGetTotalNanoseconds, OS_time_t, tm)
+shim1(int64, OS_TimeGetFractionalPart, OS_time_t, tm)
+shim1(uint32, OS_TimeGetSubsecondsPart, OS_time_t, tm)
+shim1(uint32, OS_TimeGetMillisecondsPart, OS_time_t, tm)
+shim1(uint32, OS_TimeGetMicrosecondsPart, OS_time_t, tm)
+shim1(uint32, OS_TimeGetNanosecondsPart, OS_time_t, tm)
+shim2(OS_time_t, OS_TimeAssembleFromNanoseconds, int64, seconds, uint32, nanoseconds)
+shim2(OS_time_t, OS_TimeAssembleFromMicroseconds, int64, seconds, uint32, microseconds)
+shim2(OS_time_t, OS_TimeAssembleFromMilliseconds, int64, seconds, uint32, milliseconds)
+shim2(OS_time_t, OS_TimeAssembleFromSubseconds, int64, seconds, uint32, subseconds)
+shim2(OS_time_t, OS_TimeAdd, OS_time_t, time1, OS_time_t, time2)
+shim2(OS_time_t, OS_TimeSubtract, OS_time_t, time1, OS_time_t, time2)
+
+shim1(unsigned long, OS_ObjectIdToInteger, osal_id_t, object_id)
+shim1(osal_id_t, OS_ObjectIdFromInteger, unsigned long, value)
+shim2(bool, OS_ObjectIdEqual, osal_id_t, object_id1, osal_id_t, object_id2)
+shim1(bool, OS_ObjectIdDefined, osal_id_t, object_id)
