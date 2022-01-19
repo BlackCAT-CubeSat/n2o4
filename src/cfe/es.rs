13e7c3174803e84fb6ec9e4319409f70b8d7991c
Zachary Catlin <zec0@psu.edu>
2022-01-19 18:58:20 -0500

    ES: documentation additions, improvements

--- src/cfe/es.rs
+++ src/cfe/es.rs
@@ -7,35 +7,69 @@ use super::Status;
 use printf_wrap::{PrintfFmt, PrintfArgument};
 use libc::c_char;
 
+/// The status (or requested status) of a cFE application.
 #[derive(Clone,Copy,PartialEq,Eq,Debug)]
 #[repr(u32)]
 pub enum RunStatus {
+    /// Application is exiting with an error.
     AppError = CFE_ES_RunStatus_CFE_ES_RunStatus_APP_ERROR,
+
+    /// Application wants to exit normally.
     AppExit = CFE_ES_RunStatus_CFE_ES_RunStatus_APP_EXIT,
+
+    /// Application should continue to run.
     AppRun = CFE_ES_RunStatus_CFE_ES_RunStatus_APP_RUN,
+
+    /// Indication that the Core Application could not initialize.
     CoreAppInitError = CFE_ES_RunStatus_CFE_ES_RunStatus_CORE_APP_INIT_ERROR,
+
+    /// Indication that the Core Application had a runtime failure.
     CoreAppRuntimeError = CFE_ES_RunStatus_CFE_ES_RunStatus_CORE_APP_RUNTIME_ERROR,
+
+    /// Indication that the system is requesting that the application stop.
     SysDelete = CFE_ES_RunStatus_CFE_ES_RunStatus_SYS_DELETE,
+
+    /// Application caused an exception.
     SysException = CFE_ES_RunStatus_CFE_ES_RunStatus_SYS_EXCEPTION,
+
+    /// The system is requesting a reload of the application.
     SysReload = CFE_ES_RunStatus_CFE_ES_RunStatus_SYS_RELOAD,
+
+    /// The system is requesting a restart of the application.
     SysRestart = CFE_ES_RunStatus_CFE_ES_RunStatus_SYS_RESTART,
+
+    /// Reserved value; should not be used.
     Undefined = CFE_ES_RunStatus_CFE_ES_RunStatus_UNDEFINED,
 }
 
+/// Logs an entry/exit marker for a specified ID
+/// for use by
+/// [the Software Performance Analysis tool](https://github.com/nasa/perfutils-java).
+///
+/// `marker` is a system-wide ID for some portion of code.
+/// `entry_exit` should be `0` for an entry into the code in question,
+/// and `1` for an exit.
 #[inline]
 pub fn perf_log_add(marker: u32, entry_exit: u32) {
     unsafe { CFE_ES_PerfLogAdd(marker, entry_exit) };
 }
 
+/// Shortcut for [`perf_log_add`]`(marker, 0)`.
 #[inline]
 pub fn perf_log_entry(marker: u32) { perf_log_add(marker, 0); }
 
+/// Shortcut for [`perf_log_add`]`(marker, 1)`.
 #[inline]
 pub fn perf_log_exit(marker: u32) { perf_log_add(marker, 1); }
 
 macro_rules! wtsl_impl {
     (@ $doc_end:expr, $name:ident, ( $($t:ident),* ), ( $($var:ident),* )) => {
-        #[doc = concat!("CFE_ES_WriteToSysLog with ", $doc_end)]
+        #[doc = concat!(
+            "Write a message to the cFE System Log using a format string and ",
+            $doc_end,
+            "\n\n",
+            "Wraps CFE_ES_WriteToSysLog.",
+        )]
         #[inline]
         pub fn $name<$($t),*>(fmt: PrintfFmt<($($t,)*)>, $($var: $t),*) -> Status
             where $($t: PrintfArgument),* {
@@ -67,10 +101,12 @@ wtsl_impl!( 6, write_to_syslog6, (A, B, C, D, E, F), (a, b, c, d, e, f) );
 wtsl_impl!( 7, write_to_syslog7, (A, B, C, D, E, F, G), (a, b, c, d, e, f, g) );
 wtsl_impl!( 8, write_to_syslog8, (A, B, C, D, E, F, G, H), (a, b, c, d, e, f, g, h) );
 
-/// CFE_ES_WriteToSysLog with a `str` argument.
+/// Write the contents of a [`str`] to the cFE System Log.
 ///
 /// Note that any embedded null characters and anything past them
 /// will not get put into the log message.
+///
+/// Wraps CFE_ES_WriteToSysLog.
 #[inline]
 pub fn write_to_syslog_str(msg: &str) -> Status {
     unsafe {
@@ -81,13 +117,16 @@ pub fn write_to_syslog_str(msg: &str) -> Status {
     }.into()
 }
 
+/// Exit from the current application.
+///
+/// Wraps CFE_ES_ExitApp.
 #[inline]
 pub fn exit_app(exit_status: RunStatus) {
     unsafe { CFE_ES_ExitApp(exit_status as u32) };
 }
 
 /// Check for exit requests from the cFE system,
-/// or make a request for app shutdown to the cFE system.
+/// and possibly make a request for app shutdown to the cFE system.
 ///
 /// If `run_status` is set to
 /// `Some(`[`AppExit`](`RunStatus::AppExit`)`)` or
@@ -98,9 +137,11 @@ pub fn exit_app(exit_status: RunStatus) {
 /// Returns whether the app should continue running;
 /// a return value of `false` means the application should
 /// gracefully shut down.
+///
+/// Wraps CFE_ES_RunLoop.
 #[inline]
 pub fn run_loop(run_status: Option<RunStatus>) -> bool {
-    let mut rs: u32 = run_status.map_or(0u32, |status| { status as u32 });
+    let mut rs: u32 = run_status.map_or(0, |status| { status as u32 });
     let p: *mut u32 = match run_status {
         None => core::ptr::null_mut(),
         Some(_) => &mut rs,
