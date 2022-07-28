eaf47ebc4ecb4d3d8c4dff979db333fb7d19aeec
Zachary Catlin <zec0@psu.edu>
2022-07-28 13:03:20 -0400

    Added internal-use wrapper for OS_IdentifyObject
    Still to be decided: what is the best way to expose this to users of the
    crate?

--- src/osal/mod.rs
+++ src/osal/mod.rs
@@ -192,6 +192,14 @@ impl ObjectId {
     pub fn is_defined(&self) -> bool {
         unsafe { SHIM_OS_ObjectIdDefined(self.id) }
     }
+
+    /// Returns the object type of `self` as a raw
+    /// (non-Rustic) value.
+    ///
+    /// Wraps `OS_IdentifyObject`.
+    pub(crate) fn obj_type(&self) -> osal_objtype_t {
+        unsafe { OS_IdentifyObject(self.id) }
+    }
 }
 
 /// Wraps `OS_ObjectIdFromInteger`.
@@ -221,3 +229,8 @@ impl PartialEq<Self> for ObjectId {
 }
 
 impl Eq for ObjectId {}
+
+/// Error when trying to convert an `ObjectId` to a
+/// more-specialized type.
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub struct ObjectTypeConvertError {}
