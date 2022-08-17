01475a369205bbe7a42b1a90cf340e4833a2775d
Zachary Catlin <zec0@psu.edu>
2022-08-17 14:20:51 -0400

    Initial work on SysTime; added CFE_TIME_GetTime

--- cfs/src/cfe/time.rs
+++ cfs/src/cfe/time.rs
@@ -2,3 +2,41 @@
 // SPDX-License-Identifier: Apache-2.0
 
 //! Time Services system.
+
+use cfs_sys::*;
+
+/// A system time value, represented as seconds since some epoch.
+///
+/// Wraps `CFE_TIME_SysTime_t`.
+#[doc(alias = "CFE_TIME_SysTime_t")]
+#[derive(Clone, Copy, Debug)]
+pub struct SysTime {
+    pub(crate) tm: CFE_TIME_SysTime_t,
+}
+
+impl SysTime {
+    /// Returns the number of whole seconds since the relevant epoch.
+    pub const fn seconds(self) -> u32 {
+        self.tm.Seconds
+    }
+
+    /// Returns the fractional number of seconds since the relevant epoch
+    /// (in units of 2<sup>&#8722;32</sup>&nbsp;seconds).
+    pub const fn subseconds(self) -> u32 {
+        self.tm.Subseconds
+    }
+}
+
+/// Returns the current spacecraft time,
+/// using the epoch specified in the mission configuration.
+///
+/// The time is returned either as seconds TAI or seconds UTC,
+/// depending on mission configuration.
+///
+/// Wraps `CFE_TIME_GetTime`.
+#[doc(alias = "CFE_TIME_GetTime")]
+#[inline]
+pub fn get_time() -> SysTime {
+    let tm = unsafe { CFE_TIME_GetTime() };
+    SysTime { tm }
+}
